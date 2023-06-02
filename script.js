1e73c2ade5e23c9c645855c24ae3db3856ce3f01
ivanhrudai632 <nitinhrudai2002@gmail.com>
2023-06-02 20:53:11 +0530

    hello

---
+++ script.js
@@ -0,0 +1,144 @@
+
+const stars = document.getElementsByClassName("fa-regular fa-star");
+
+stars[0].onclick = function(){
+    stars[0].style.color ="rgb(172, 43, 247)";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[1].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="rgb(172, 43, 247)";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[2].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="rgb(172, 43, 247)";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[3].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="rgb(172, 43, 247)";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[4].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="rgb(172, 43, 247)";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[5].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="rgb(172, 43, 247)";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[6].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="rgb(172, 43, 247)";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[7].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="rgb(172, 43, 247)";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[8].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="rgb(172, 43, 247)";
+    stars[9].style.color ="e4e4e4";
+    
+}
+
+stars[9].onclick = function(){
+    stars[0].style.color ="e4e4e4";
+    stars[1].style.color ="e4e4e4";
+    stars[2].style.color ="e4e4e4";
+    stars[3].style.color ="e4e4e4";
+    stars[4].style.color ="e4e4e4";
+    stars[5].style.color ="e4e4e4";
+    stars[6].style.color ="e4e4e4";
+    stars[7].style.color ="e4e4e4";
+    stars[8].style.color ="e4e4e4";
+    stars[9].style.color ="rgb(172, 43, 247)";
+    
+}
+
+
